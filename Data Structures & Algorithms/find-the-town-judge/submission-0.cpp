e54d83ce8e3eeefe722f4509edf10b46c8a81d83
James_Hsieh <e4803703d@gmail.com>
2026-07-21 13:01:16 +0800

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> adj(n+1, 0);
+        for(auto& a:trust){
+            adj[a[1]]++;
+            adj[a[0]]--;
+        }
+        for(int i=1; i<=n; i++){
+            if(adj[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
