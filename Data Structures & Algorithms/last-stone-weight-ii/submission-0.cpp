df244091c8a93d3fbc934f28c2bb4ddd6c5950d4
James_Hsieh <e4803703d@gmail.com>
2026-08-10 12:52:25 +0800

    Add: last-stone-weight-ii - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum=0;
+        for(int& x:stones) sum+=x;
+        int target=sum/2;
+        vector<int> dp(target+1, 0);
+        for(int& x:stones){
+            for(int i=target; i>=x; i--){
+                dp[i]=max(dp[i], dp[i-x]+x);
+            }
+        }
+        return sum-2*dp.back();
+    }
+};
